3ce4bfafbc48cbb291a515de1f14c8c411c690ab
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-06-14 14:57:09 +0300

    fix r code rus models in fevd

--- russian_paper/model_1/fevd_table.R
+++ russian_paper/model_1/fevd_table.R
@@ -3,10 +3,9 @@ library(vars)
 load("russian_paper_results/model_1/svar_results.RData")
 
 HORIZONS <- c(1, 2, 3, 12)
-H_INF    <- 500
+H_INF <- 500
 
-# --- 1. FEVD (Холецкий внутри, как и в irf(ortho = TRUE)) -----
-fe <- fevd(var_model, n.ahead = H_INF)$r            # H_INF x K, доли
+fe <- fevd(var_model, n.ahead = H_INF)$r
 
 tab <- fe[c(HORIZONS, H_INF), ] * 100
 colnames(tab) <- c("Oil Supply Shock",
@@ -15,18 +14,9 @@ colnames(tab) <- c("Oil Supply Shock",
                    "Other Shocks")
 rownames(tab) <- c(HORIZONS, "Inf")
 
-# Контроль сходимости на "бесконечности": вклад на h = 400 и
-# h = 500 должен совпадать, иначе увеличить H_INF
+# проверим, что на большом горизонте сходятся результаты
 stopifnot(max(abs(fe[400, ] - fe[H_INF, ])) < 1e-4)
 
-# --- 2. Вывод -------------------------------------------------
-cat("\nTable 1. Percent contribution to the variability",
-    "of Russian real stock returns (Russian Model v1)\n\n")
+# полученная таблица
 print(round(tab, 2))
-
-oil_total <- rowSums(tab[, 1:3])
-cat("\nСуммарный вклад нефтяных шоков по горизонтам:\n")
-print(round(oil_total, 2))
-
 write.csv(round(tab, 2), "russian_paper_results/model_1/fevd_stock_returns.csv")
-cat("\nСохранено: fevd_stock_returns.csv\n")
