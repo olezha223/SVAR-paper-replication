861c84cfc22e7f0feffc87b069b2debf2130f4a5
Oleg Shvetsov <118572008+olezha223@users.noreply.github.com>
2026-06-14 03:21:00 +0300

    забагованный код построения импульсных откликов по отраслям

---
+++ russian_paper/model_industries/industry_svar.r
@@ -0,0 +1,25 @@
+library(readxl)
+library(vars)
+
+# --- 1. Данные ------------------------------------------------
+df <- read_excel("russian_data/processed/DATASET-industries.xlsx")
+
+# Возможные варианты: consumer_log_return,metals_mining_log_return,oil_gas_log_return
+var_name <- "oil_gas_log_return"   # <--- МЕНЯЙТЕ ЗДЕСЬ НАЗВАНИЕ ПЕРЕМЕННОЙ
+df[[var_name]] <- df[[var_name]] * 100
+
+yt <- as.matrix(df[, c("delta_non_rus", "rea_t", "rpo", var_name)])
+colnames(yt) <- c("delta_prod", "rea", "rpo", var_name)
+
+stopifnot(!anyNA(yt))
+cat("Наблюдений:", nrow(yt), "\n")
+
+var_model <- VAR(yt, p = 24, type = "const")
+
+P <- t(chol(summary(var_model)$covres))
+cat("\nИмпакт-матрица (one-SD шоки):\n")
+print(round(P, 4))
+
+industry_dir <- file.path("russian_paper_results", "industry_results")
+output_file <- file.path(industry_dir, paste0(var_name, ".RData"))
+save(var_model, file = output_file)
